0679f2368f13f9da7bbb527639334007e8658c6d
Fatimah <daviesfatimah97@gmail.com>
2021-08-19 19:00:49 -0700

    fix routing

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
