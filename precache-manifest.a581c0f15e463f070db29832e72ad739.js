8459d50f5f91487fb58c2e4c2748fd631867b889
Fatimah019 <daviesfatimah97@gmail.com>
2025-03-23 22:38:06 +0100

    Updates

---
+++ precache-manifest.a581c0f15e463f070db29832e72ad739.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b1aea67c7397a4c0942fc227313f7d",
+    "url": "/Me/index.html"
+  },
+  {
+    "revision": "86b3e2343e7a1a7d9adc",
+    "url": "/Me/static/css/main.0a106d17.chunk.css"
+  },
+  {
+    "revision": "8e5af326b004581ee292",
+    "url": "/Me/static/js/2.1280c0f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Me/static/js/2.1280c0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b3e2343e7a1a7d9adc",
+    "url": "/Me/static/js/main.9816051a.chunk.js"
+  },
+  {
+    "revision": "b43eb9c2633cab110ccf",
+    "url": "/Me/static/js/runtime-main.09d99b8d.js"
+  },
+  {
+    "revision": "896ee119ce87432ac11be80f4f2c22a9",
+    "url": "/Me/static/media/Axiforma-Heavy.896ee119.ttf"
+  },
+  {
+    "revision": "b37fe49c17e87a2a11d6ef49c1d8e1e5",
+    "url": "/Me/static/media/Axiforma-Heavy.b37fe49c.woff"
+  },
+  {
+    "revision": "f131e9db5ed90b945937a678b685ebee",
+    "url": "/Me/static/media/Axiforma-Heavy.f131e9db.eot"
+  },
+  {
+    "revision": "6fba87fa77ced9fdae6dc96fa70a1586",
+    "url": "/Me/static/media/Axiforma-Light.6fba87fa.ttf"
+  },
+  {
+    "revision": "abea748a05feed2e0043572711f075d9",
+    "url": "/Me/static/media/Axiforma-Light.abea748a.woff"
+  },
+  {
+    "revision": "c758bbe3c3918125a5b5c000a4c8c92b",
+    "url": "/Me/static/media/Axiforma-Light.c758bbe3.eot"
+  },
+  {
+    "revision": "73e6697756652f69fc9cd5af01a39c84",
+    "url": "/Me/static/media/Axiforma-Regular.73e66977.eot"
+  },
+  {
+    "revision": "b4afda971855a50bfaaf56e6cd6b1f85",
+    "url": "/Me/static/media/Axiforma-Regular.b4afda97.woff"
+  },
+  {
+    "revision": "d882de2cc508ece40b39e18c754dd9f3",
+    "url": "/Me/static/media/Axiforma-Regular.d882de2c.ttf"
+  }
+]);
